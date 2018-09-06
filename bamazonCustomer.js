35defa8bb8a88602d5ee2a4d283675fca469d19d
tywi6665 <tyletcole@gmail.com>
2018-09-06 14:37:05 -0600

    push

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,30 +11,35 @@ var connection = mysql.createConnection ({
     database: "bamazon"
 });
 
+//Use mySql connection and start basic functionality
 connection.connect (function (err) {
     if (err) throw err;
     start ();
 });
 
+//Call the start function to display item inventory
 function start () {
     console.log ("\nScrew Christmas, Fall is the most wonderful time fo the year. Now buy some stuff!!!");
+    //Query mySQL to retreive item inventory
     connection.query ("SELECT * FROM products", function (err, res) {
         if (err) throw err;
-       
+        //For each member of the of the database console.log out unique row, which is readable to user
         res.forEach (function (product) {
             console.log ("\n" + product.product_name + " | " + " (Item ID: " + product.item_id + ")  | " + "$" + product.price);
         });
-
+        //call the inquirer function
         userExperience ();
     });
 };
     
 function userExperience () {
+    //Uses inquirer to prompt user
     inquirer.prompt ([
         {
             message: "\nWhat's the ID of the product that speaks to you the most?",
             type: "input",
             name: "productID",
+            //Ensures user can only input a number
             validate: function (value) {
                 if (isNaN (value) == false) {
                     return true;
@@ -57,18 +62,24 @@ function userExperience () {
         }
     ])
     .then (function (response) {
+        //queries the database again, but this time specifically for item IDs
         connection.query ("SELECT * FROM products WHERE item_id = " + response.productID, function (err, res) {
             if (err) throw err;
+            //Retrieve quantity and pricing info from database
             var productQuantity = res[0].stock_quantity;
             var productPrice = res[0].price;
+            //If user inputs a higher quantity for an item than exists in the database:
             if (response.quantity > productQuantity) {
                 console.log ("\nYou're way too needy for your own good");
+                console.log ("----------------------------------------------");
                 start ();
+            //If desired user quantity is lower or equal to inventory amount:
             } else {
                 var updatedQuantity = productQuantity - response.quantity;
                 connection.query ("UPDATE products SET ? Where ?", 
                 [
                     {
+                        //Updates database amount
                         stock_quantity: updatedQuantity
                     },
                     {
@@ -76,6 +87,7 @@ function userExperience () {
                     }
                 ], function (err, res) {
                     if (err) throw err;
+                    //Calcualtes total cost for user
                     var totalCost = response.quantity * productPrice;
                     console.log ("\nTime to pay up!! You owe $" + totalCost);
                     console.log ("----------------------------------------------");
